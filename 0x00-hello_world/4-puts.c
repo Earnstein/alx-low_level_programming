969b8d0b59c9c9936b1ff1dd3e441a9a4278061d
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-09-08 11:59:40 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
-#!/bin/bash
-
 #include <stdio.h>
+
+
 
 /**
  *
- * main - Entry point
+ *  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
@@ -15,6 +18,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-		return (0);
+			return (0);
 
 }
