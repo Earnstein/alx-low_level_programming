faa74311240ad9378fbb8c24554f01cf015ef1f4
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-09-08 14:41:52 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
+/*
+ * file: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19" - Dora Korpar, 2015-10-19",
+ * Return: Always 1.
+ *
  */
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2,
+			"and that piece of art is useful\"- Dora Korpar,
+		2015-10-19\n",
+		59);
 
 		return (1);
 }
