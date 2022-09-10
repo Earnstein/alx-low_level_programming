73edb6e3dbc0551dde6b17592467f3f9d5527ff2
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-09-09 17:54:02 -0700

    .

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
- * Result: 0 on success
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char c = 'z';
+	char low;
 
-	while (c >= 'a')
-	{
-		putchar(c);
-		c--;
-	}
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
 	putchar('\n');
 	return (0);
 }
