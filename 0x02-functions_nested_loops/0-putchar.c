69cdd362b116d1898c0d37235488be00a08b4af3
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-09-13 08:47:26 -0700

    .

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success always
+ */
+
+int main(void)
+{
+	char dami[] = "_putchar";
+
+	for (int i = 0; i < 9; i++)
+	{
+		_putchar(dami[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
