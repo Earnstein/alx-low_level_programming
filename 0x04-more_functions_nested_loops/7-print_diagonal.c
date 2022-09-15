e95983a8f0d016c8b315f56ed9416861585c1355
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-09-15 15:54:53 -0700

    .

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line using "\" character of n length"
+ * @n: length of the line
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
