fbc7ea45ce4cb13aa2e35a0896a5966d7ea6481e
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-09-15 08:22:48 -0700

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks if an integer is positive or negative and prints it
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
