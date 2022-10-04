3977574b9357ac805e67c383e17529874477f158
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-10-04 14:51:56 -0700

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	char_array = malloc(sizeof(char) * size);
 	if (char_array == NULL)
-		reyurn (NULL);
+		return (NULL);
 	for (i = 0; i < size; i++)
 		char_array[i] = c;
 	return (char_array);
