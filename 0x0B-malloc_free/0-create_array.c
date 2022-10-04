18e5d8228830008a205afda980a12872ac617eaf
Bakare Damilola Ayomide <bdamilola00@gmail.com>
2022-10-04 14:49:40 -0700

    Learing memory proccing in C

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char
+ * @size: size of array
+ * @c: character to init array
+ * Return: char pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *char_array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	char_array = malloc(sizeof(char) * size);
+	if (char_array == NULL)
+		reyurn (NULL);
+	for (i = 0; i < size; i++)
+		char_array[i] = c;
+	return (char_array);
+}
